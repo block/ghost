5665b6479f02ed4e5dbe555af875e6ead13f7e6a
Nahiyan Khan <nahiyan@squareup.com>
2026-06-08 10:18:24 -0400

    Add fingerprint-first public exports

--- packages/ghost/src/index.ts
+++ packages/ghost/src/index.ts
@@ -1,4 +1,12 @@
+import * as compareApi from "./compare.js";
+import { compare as compareFunction } from "./core/index.js";
+
+/** @deprecated Use `govern`, `compare`, `@anarchitecture/ghost/govern`, or `@anarchitecture/ghost/compare`. */
 export * as drift from "./core/index.js";
 export * from "./core/index.js";
+export const compare = Object.assign(compareFunction, compareApi);
+export * as fingerprint from "./fingerprint.js";
 export * as core from "./ghost-core/index.js";
+export * as govern from "./govern.js";
+/** @deprecated Use `fingerprint` or `@anarchitecture/ghost/fingerprint`. */
 export * as scan from "./scan/index.js";
